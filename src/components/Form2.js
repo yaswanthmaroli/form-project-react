c44f7f10ec0373e25d4f03cbf65b3751bbea1967
Yaswanth <myaswanth831@gmail.com>
2023-01-30 01:50:39 +0530

    last deploy

--- src/components/Form2.js
+++ src/components/Form2.js
@@ -3,35 +3,71 @@ import { useState } from "react";
 import { Container, Row, Col, Table } from "reactstrap";
 import "../style/Form2.css";
 
+
 const Form2 = () => {
+  const [formerror, setformerror] = useState({});
   const [inputarr, setinputarr] = useState([]);
+  const [edit, setEdit] = useState(false);
+
   const [inputdata, setinputdata] = useState({
     name: "",
     email: "",
     phone: "",
   });
 
-  function changeHandle(e) {
+  let contentEdit = (e) => {
+    e.preventDefault();
+    setEdit(!edit);
+  };
+
+  const changeHandle = (e) => {
     setinputdata({ ...inputdata, [e.target.name]: e.target.value });
-  }
+  };
+
+  const validateForm = () => {
+    let err = {};
+
+    if (inputdata.name === "") {
+      err.name = "Name is required";
+    }
+    if (inputdata.email === "") {
+      err.email = "email is required";
+    }
+    if (inputdata.name === "") {
+      err.phone = "phone is required";
+    }
+
+    setformerror({ ...err });
+
+    return Object.keys(err).length < 1;
+  };
 
   let { name, email, phone } = inputdata;
-  function addinputdata(e) {
+  const addinputdata = (e) => {
     e.preventDefault();
     setinputarr([...inputarr, { name, email, phone }]);
-    console.log(inputarr.length);
-    console.log(inputdata);
+
     setinputdata({ name: "", email: "", phone: "" });
-  }
+
+    console.log(inputarr.length);
+
+    let isvalid = validateForm();
+    console.log(isvalid);
+    if (isvalid) {
+      alert("submitted");
+    } else {
+      alert("invalid form");
+    }
+  };
 
   return (
     <div className="form2">
-      <form>
+      <form onSubmit={addinputdata}>
         <div>
           <Container className="form-items">
             <Col xs="12" md="12">
               <div className="form2-btn">
-                <button onClick={addinputdata} className="addrow-btn ">
+                <button type="submit" className="addrow-btn ">
                   Save
                 </button>
               </div>
@@ -43,8 +79,11 @@ const Form2 = () => {
                 name="name"
                 type="text"
                 placeholder="name"
-                onChange={changeHandle}
+                onChange={(e) => changeHandle(e)}
               />
+              <div className="name-validation">
+                <span>{formerror.name}</span>
+              </div>
             </Col>
 
             <Col xs="12" md="12" className="form2-input">
@@ -54,8 +93,12 @@ const Form2 = () => {
                 name="email"
                 type="email"
                 placeholder="email"
-                onChange={changeHandle}
+                onChange={(e) => changeHandle(e)}
               />
+              <div className="name-validation">
+                {" "}
+                <span>{formerror.email}</span>
+              </div>
             </Col>
 
             <Col xs="12" md="12" className="form2-input">
@@ -65,22 +108,32 @@ const Form2 = () => {
                 name="phone"
                 type="text"
                 placeholder="phone"
-                onChange={changeHandle}
+                onChange={(e) => changeHandle(e)}
               />
+              <div className="name-validation">
+                {" "}
+                <span>{formerror.phone}</span>
+              </div>
             </Col>
           </Container>
 
-          {
-            inputarr.length  > 0  && (<Container className="form-display">
-            <Col xs="12" md="12">
-              <div className="form2-btn">
-                <button className="editrow-btn">Edit</button>
-              </div>
-            </Col>
-            
-            {
-               inputarr.map((item,i)=>(
-                <Row>
+          {inputarr.length > 0 && (
+            <Container className="form-display">
+              <Col xs="12" md="12">
+                <div className="form2-btn">
+                  {edit ? (
+                    <button className="editrow-btn" onClick={contentEdit}>
+                      save
+                    </button>
+                  ) : (
+                    <button className="editrow-btn " onClick={contentEdit}>
+                      edit
+                    </button>
+                  )}
+                </div>
+              </Col>
+
+              <Row>
                 <div className="form-display-items">
                   <Col md="10">
                     <Table bordered>
@@ -91,25 +144,22 @@ const Form2 = () => {
                           <th>Mobile</th>
                         </tr>
                       </thead>
-  
-                      <tbody>
-                        <tr>
-                          <td> {item.name}</td>
-                          <td> {item.email} </td>
-                          <td> {item.phone} </td>
-                        </tr>
-                      </tbody>
+
+                      {inputarr.map((item, i) => (
+                        <tbody>
+                          <tr contentEditable={edit}>
+                            <td> {item.name}</td>
+                            <td> {item.email} </td>
+                            <td> {item.phone} </td>
+                          </tr>
+                        </tbody>
+                      ))}
                     </Table>
-                  </Col>
-                  <Col md="2">
-                    <button className="deleterow-btn">Delete</button>
                   </Col>
                 </div>
               </Row>
-              ))
-            }
-          </Container>)
-          }
+            </Container>
+          )}
         </div>
       </form>
     </div>
